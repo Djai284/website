084883e6357649421720c83a0fae670ab534dab9
Dhananjai <dhananjai284@gmail.com>
2025-02-12 15:04:27 -0500

    added resume route

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+const ResumePage = () => {
+  useEffect(() => {
+    window.location.href = "https://firebasestorage.googleapis.com/v0/b/personal-website-f0071.appspot.com/o/Dhananjai%20Resume.pdf?alt=media&token=d0e1625e-4780-40ef-9081-9209c7adc270";
+  }, []);
+
+  return null;
+}
+
+export default ResumePage;
