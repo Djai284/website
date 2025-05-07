2754771a9dc46cce90d84c0b748eb7314c466fa4
Dhananjai <dhananjai284@gmail.com>
2025-05-06 22:48:31 -0400

    updated resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 const ResumePage = () => {
   useEffect(() => {
-    window.location.href = "https://firebasestorage.googleapis.com/v0/b/personal-website-f0071.appspot.com/o/Dhananjai%20Resume.pdf?alt=media&token=d0e1625e-4780-40ef-9081-9209c7adc270";
+    window.location.href = "https://firebasestorage.googleapis.com/v0/b/personal-website-f0071.appspot.com/o/Dhananjai_Resume.pdf?alt=media&token=7ed94f15-b2a5-48e1-b4c1-0a3a69395401";
   }, []);
 
   return null;
